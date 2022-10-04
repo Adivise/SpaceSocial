6c1a886df18a98e19254e000ceebb0066c701844
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-10-04 16:16:12 +0530

    `Added perm to addmoney`

--- commands/settings/admin.js
+++ commands/settings/admin.js
@@ -57,7 +57,7 @@ module.exports = {
     ],
     run: async (interaction, client) => {
         await interaction.deferReply({ ephemeral: false });
-        if (!interaction.member.permissions.has('ManageGuild')) return interaction.editReply("You need the **MANAGE_GUILD** permission!"); {
+        if (!interaction.member.permissions.has('ManageGuild')) return interaction.editReply("You need the **MANAGE_GUILD** permission!");
         if (interaction.options.getSubcommand() === "give") {
             const args = interaction.options.getInteger("amount");
     
@@ -106,7 +106,6 @@ module.exports = {
 
     }
     }
-}
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
